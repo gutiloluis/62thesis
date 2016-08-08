2d00d2c03db42dae68e974458d077681a30b750c
gutiloluis <gutiloluis@gmail.com>
2016-08-08 15:24:02 -0500

    first

---
+++ ta1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_rng.h>
+
+int i;
+const gsl_rng_type *T;
+gsl_rng *R;
+
+int main(){
+
+
+
+	return 0;
+
+}
+
+void step(double *t, double *d, double *r, double *a, double *b, double *ab, double ){
+
+}

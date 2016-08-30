8c80762d84e48fcc769ae5c97f89fec40a70fdf0
gutiloluis <gutiloluis@gmail.com>
2016-08-30 11:39:41 -0500

    eternal ifs

--- mara/ta11.c
+++ mara/ta11.c
@@ -67,8 +67,7 @@ r2s: MarR_2 bound to salicylate.
 13. au degradation
 14. a degradation
 15. ru degradation
-16. r degradation
-17. r2 degradation
+16. (2) r degradation (with r2)
 18. r2 ans s binding
 19. r2s unbinding
 */
@@ -76,12 +75,199 @@ r2s: MarR_2 bound to salicylate.
 
 void step(double *t, double *p00, double *p01, double *p02, double *p10, double *p11, double *p12, double *m, double *au, double *a, double *ru, double *r, double *r2, double *s, double *r2s){
 
-	double K = ( ka_u**p10 + ka_u**p11 + ka_u**p12 )  +  ( ka**p00**a/v + ka**p01**a/(v*b) + ka**p02**a/(v*b_p) )  +  ( kr_u**p01 + 2.0*kr_u**p02 + kr_u**p11 + 2.0*kr_u**p12 )  +  ( 2.0*kr**p00**r2/v + kr**p01**r2/v + 2.0*kr**p10**r2/(v*al)  +  kr**p11**r2/(v*al_p) )  +  ( al00**p00 + al01**p01 + al10**p10 + al11**p11 + al12**p12 + al02**p02 )  +  ( l_m**m )  +  ()
+	double K = ( ka_u**p10 + ka_u**p11 + ka_u**p12 )  +  ( ka**p00**a/v + ka**p01**a/(v*b) + ka**p02**a/(v*b_p) )  +  ( kr_u**p01 + 2.0*kr_u**p02 + kr_u**p11 + 2.0*kr_u**p12 )  +  ( 2.0*kr**p00**r2/v + kr**p01**r2/v + 2.0*kr**p10**r2/(v*al)  +  kr**p11**r2/(v*al_p) )  +  ( al00**p00 + al01**p01 + al10**p10 + al11**p11 + al12**p12 + al02**p02 )  +  ( l_m**m )  +  ( b_a**m )  +  ( b_r**m )  +  ( kf_a**au )  +  ( kf_r**ru )  +  ( kd_r**r*(*r-1)/(v*2.0) )  +  ( kd_ru**r2 )  +  ( l_au**au )  +  ( l_a**a )  +  ( l_ru**ru )  +  ( l_r**r + l_r2**r2 )  +  ( ks**r2**s/v )  +  ( ks_u**r2s );
 
 	double Dt = -log(gsl_rng_uniform(R))/K;
-  double which = gsl_rng_uniform(R);
+  double w = gsl_rng_uniform(R);
 
 	*t+=Dt;
+
+	/* a dissociation from p10 */
+	if( w < ka_u**p10/K ){
+		*p10 -= 1;
+		*p00 += 1;
+		*a += 1;
+	}
+	/* a dissociation from p11 */
+	else if( w >= ka_u**p10/K && w < ( ka_u**p10 + ka_u**p11 )/K ){
+		*p11 -= 1;
+		*p01 += 1;
+		*a += 1;
+	}
+	/* a dissociation from p12 */
+	else if( w >= ( ka_u**p10 + ka_u**p11 )/K && w < ( ka_u**p10 + ka_u**p11 + ka_u**p12 )/K ){
+		*p12 -= 1;
+		*p02 += 1;
+		*a += 1;
+	}
+	/*a association to p00 */
+	else if( w >= ( ka_u**p10 + ka_u**p11 + ka_u**p12 )/K && w < ( (ka_u**p10 + ka_u**p11 + ka_u**p12) + (ka**p00**a/v) )/K){
+		*p00 -= 1;
+		*p10 += 1;
+		*a -= 1;
+	}
+	/*a association to p01 */
+	else if( w >= ( (ka_u**p10 + ka_u**p11 + ka_u**p12) + (ka**p00**a/v) )/K && w < ( (ka_u**p10 + ka_u**p11 + ka_u**p12) + (ka**p00**a/v + ka**p01**a/(v*b)) )/K ){
+		*p01 -= 1;
+		*p11 += 1;
+		*a -= 1;
+	}
+	/*a association to p02 */
+	else if( w >= ( (ka_u**p10 + ka_u**p11 + ka_u**p12) + (ka**p00**a/v + ka**p01**a/(v*b)) )/K && w < ( (ka_u**p10 + ka_u**p11 + ka_u**p12) + (ka**p00**a/v + ka**p01**a/(v*b) + ka**p02**a/(v*b_p)) )/K ){
+		*p02 -= 1;
+		*p12 += 1;
+		*a -= 1;
+	}
+	/*r2 dissociation from p01 */
+	else if( w >= ( (ka_u**p10 + ka_u**p11 + ka_u**p12) + (ka**p00**a/v + ka**p01**a/(v*b) + ka**p02**a/(v*b_p)) )/K && w < ( (ka_u**p10 + ka_u**p11 + ka_u**p12) + (ka**p00**a/v + ka**p01**a/(v*b) + ka**p02**a/(v*b_p)) + (kr_u**p01) )/K ){
+		*p01 -= 1;
+		*p00 += 1;
+		*r2 += 1;
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+		*p02 -= 1;
+		*p01 += 1;
+		*r2 += 1;
+	}
+	/*r2 dissociation from p11 */
+	else if(  ){
+		*p11 -= 1;
+		*p10 += 1;
+		*r2 += 1;
+	}
+	/*r2 dissociation from p12 */
+	else if(  ){
+		*p12 -= 1;
+		*p11 += 1;
+		*r2 += 1;
+	}
+	/*r2 association to p00 */
+	else if(  ){
+		*p00 -= 1;
+		*p01 += 1;
+		*r2 -= 1;
+	}
+	/*r2 association to p01 */
+	else if(  ){
+		*p01 -= 1;
+		*p02 += 1;
+		*r2 -= 1;
+	}
+	/*r2 association to p10 */
+	else if(  ){
+		*p10 -= 1;
+		*p11 += 1;
+		*r2 -= 1;
+	}
+	/*r2 association to p11 */
+	else if(  ){
+		*p11 -= 1;
+		*p12 += 1;
+		*r2 -= 1;
+	}
+	/*transcription with p00 */
+	else if(  ){
+
+	}
+	/*transcription with p00 */
+	else if(  ){
+
+	}
+	/*transcription with p00 */
+	else if(  ){
+
+	}
+	/*transcription with p00 */
+	else if(  ){
+
+	}
+	/*transcription with p00 */
+	else if(  ){
+
+	}
+	/*transcription with p00 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+	/*r2 dissociation from p02 */
+	else if(  ){
+
+	}
+
+
 
 	if(which < k_r**d/K){
 		*r+=1;
@@ -89,30 +275,6 @@ void step(double *t, double *p00, double *p01, double *p02, double *p10, double 
 		*a+=1;
 	} else if(which >= (k_r**d + k_a**r)/K && which < (k_r**d + k_a**r + k_b**r)/K){
 		*b+=1;
-	} else if(which >= (k_r**d + k_a**r + k_b**r)/K && which < (k_r**d + k_a**r + k_b**r + kon_ab**a**b)/K){
-		*a-=1;
-		*b-=1;
-		*ab+=1;
-	} else if(which >= (k_r**d + k_a**r + k_b**r + kon_ab**a**b)/K && which < (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab)/K){
-		*a+=1;
-		*b+=1;
-		*ab-=1;
-	} else if(which >= (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab)/K && which < (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab + kb_dab**d**ab)/K){
-		*d-=1;
-		*ab-=1;
-		*dab+=1;
-		k_r*=k_r_l;
-	} else if(which >= (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab + kb_dab**d**ab)/K && which < (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab + kb_dab**d**ab + ku_dab**dab)/K){
-		*d+=1;
-		*ab+=1;
-		*dab-=1;
-		k_r/=k_r_l;
-	} else if(which >= (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab + kb_dab**d**ab + ku_dab**dab)/K && which < (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab + kb_dab**d**ab + ku_dab**dab + g_r**r)/K){
-		*r-=1;
-	} else if(which >= (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab + kb_dab**d**ab + ku_dab**dab + g_r**r)/K && which < (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab + kb_dab**d**ab + ku_dab**dab + g_r**r + g_a**a)/K){
-		*a-=1;
-	} else if(which >= (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab + kb_dab**d**ab + ku_dab**dab + g_r**r + g_a**a)/K && which < (k_r**d + k_a**r + k_b**r + kon_ab**a**b + koff_ab**ab + kb_dab**d**ab + ku_dab**dab + g_r**r + g_a**a + g_b**b)/K){
-		*b-=1;
 	} else {
 		*ab-=1;
 	}
